58fa5f05a5daaeaf9caf3c0769071870dd8e5d5d
venkatbajaj <89638979+venkatbajaj@users.noreply.github.com>
2021-09-07 23:25:19 +0530

    Add files via upload

---
+++ 3_Implementation/test/test.c
@@ -0,0 +1,47 @@
+#include "unity.h"
+#include "fun.h"
+#define PROJECT_NAME "LibraryManagement"
+
+void test_displaycomplete(void);
+void test_addbook(void);
+void test_search(void);
+void test_stock(void);
+
+void setUp()
+{
+}
+void tearDown()
+{
+}
+
+/* Start of the application test */
+int main()
+{
+/* Initiate the Unity Test Framework */
+  UNITY_BEGIN();
+
+/* Run Test functions */
+  RUN_TEST(test_addbook);
+  RUN_TEST(test_displaycomplete);
+  RUN_TEST(test_search);
+  RUN_TEST(test_stock);
+
+   return UNITY_END();
+}
+
+void test_addbook(void)
+{
+  TEST_ASSERT_EQUAL(Found,addbook("Books.txt"));
+}
+void test_displaycomplete(void)
+{
+  TEST_ASSERT_EQUAL(Found,displaycomplete("Books.txt"));
+}
+void test_search(void)
+{
+  TEST_ASSERT_EQUAL(Found,search("Books.txt"));
+}
+void test_stock(void)
+{
+  TEST_ASSERT_EQUAL(Found,stock("Books.txt"));
+}
