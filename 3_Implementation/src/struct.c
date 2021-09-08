1592de2bdb9dda39be868f68f8aa2694c786a92f
venkatbajaj <89638979+venkatbajaj@users.noreply.github.com>
2021-09-08 15:43:42 +0530

    Add files via upload

---
+++ 3_Implementation/src/struct.c
@@ -0,0 +1,14 @@
+#include "lms.h"
+
+struct book                                                   
+    {
+        int bookid;
+        char booktitle[200];
+        char bookauthor[200];
+        int stockavailable;
+    };
+struct student
+{
+    int id;
+    char Student_name[100];
+}student; 
